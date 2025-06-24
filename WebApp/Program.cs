62f330d0b42d3cd4d957ff3fb13a555bcb291ac0
Damla Metin Orak <damla91@gmail.com>
2025-06-24 22:49:44 +0300

    Custom Parameter Constraints

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,6 +1,12 @@
 using Microsoft.AspNetCore.Routing.Matching;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRouting(options =>
+{
+    options.ConstraintMap.Add("pos", typeof(PositionConstraint));
+});
+
 var app = builder.Build();
 
 app.Use(async (context, next) =>
@@ -37,21 +43,32 @@ app.UseEndpoints(endpoints =>
         await context.Response.WriteAsync($"Delete the employee: {context.Request.RouteValues["id"]}");
     });
 
-    endpoints.MapGet("/{category=shirts}/{size=medium}/{id?}", async (HttpContext context) =>
-    {
-        await context.Response.WriteAsync($"Get category: {context.Request.RouteValues["category"]} in size: {context.Request.RouteValues["size"]} with the id: {context.Request.RouteValues["id"]}");
-    });
-
     endpoints.MapGet("/employees/{id:int}", async (HttpContext context) =>
     {
         await context.Response.WriteAsync($"Get employee: {context.Request.RouteValues["id"]}");
     });
 
-    endpoints.MapGet("/employees/{name}", async (HttpContext context) =>
+    endpoints.MapGet("/employees/positions/{position:pos}", async (HttpContext context) =>
     {
-        await context.Response.WriteAsync($"Get employee: {context.Request.RouteValues["name"]}");
+        await context.Response.WriteAsync($"Get employees under position: {context.Request.RouteValues["position"]}");
     });
 });
 
 
 app.Run();
+
+
+class PositionConstraint : IRouteConstraint
+{
+    public bool Match(HttpContext? httpContext, IRouter? route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+    {
+        if (!values.ContainsKey(routeKey)) return false;
+
+        if (values[routeKey] is null) return false;
+
+        if(values[routeKey].ToString().Equals("manager", StringComparison.OrdinalIgnoreCase) || 
+            values[routeKey].ToString().Equals("developer", StringComparison.OrdinalIgnoreCase)) return true;
+
+        return false;
+    }
+}
